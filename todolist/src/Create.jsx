729ac2ff942ef5c47b0d64fdbb7eedfbed09651b
Obua Stella <stellaobua123@gmail.com>
2024-01-02 03:39:11 +0100

    Setup Server

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,10 @@
+function Create() {
+  return (
+    <div className="create_form">
+      <input type="text" name="" id="" placeholder />
+      <button type="submit">Add</button>
+    </div>
+  );
+}
+
+export default Create;
---
+++ todolist/src/Home.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Create from "./Create";
+
+function Home() {
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="home">
+      <h2>Todo List</h2>
+      <Create />
+      {todos.length === 0 ? (
+        <div>
+          <h2>No records</h2>
+        </div>
+      ) : (
+        todos.map((todo) => <div>{todo}</div>)
+      )}
+    </div>
+  );
+}
+
+export default Home;
