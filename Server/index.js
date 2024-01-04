c95952e837191f84bedc2e60663165eb571119ec
Obua Stella <stellaobua123@gmail.com>
2024-01-04 16:42:32 +0100

    Setup server

--- Server/index.js
+++ Server/index.js
@@ -3,3 +3,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+app.use(cors()); // cors enables us to connect our frontend with the backend.
+app.use(express.json()); // This means whenever we pass data it will be converted to the json format
+
+app.listen(3001, () => {
+  console.log("Server is Running");
+});
